28ceef56e0c325425f5635ffe91b217a112d532a
u10000129 <u10000129@gmail.com>
2017-06-15 18:56:02 +0800

    add update code

--- parse_speed_sensor.py
+++ parse_speed_sensor.py
@@ -4,19 +4,18 @@
 import psycopg2
 
 # read file
-f = open("Speed-Measuring Sensor", "r")
+f = open("Speed-Measuring-Sensor_1", "r")
 conn = psycopg2.connect(database="db2", user="db2", password="db002", host="140.114.77.23", port="5432")
 print "Opened database successfully"
 cur = conn.cursor()
-'''
-f.readline()
 f.readline()
 for line in f.readlines():
     line = line.split()
-    cur.execute("INSERT INTO speed_measuring_sensor (Item_NO,Road_ID,Road_Direction,Milage,Speed_Limit,Latitude,Longitude) VALUES ("+line[0]+", '"+line[2]+"', '"+line[3]+"', "+line[4]+", "+line[5]+", "+line[6]+", "+line[7]+")")
-'''
+    #cur.execute("INSERT INTO speed_measuring_sensor (Item_NO,Road_ID,Road_Direction,Milage,Speed_Limit,Latitude,Longitude) VALUES ("+line[0]+", '"+line[2]+"', '"+line[3]+"', "+line[4]+", "+line[5]+", "+line[6]+", "+line[7]+")")
+    cur.execute("UPDATE speed_measuring_sensor set location_path =" + line[8] + " where Item_No="+line[0])
 conn.commit()
 cur.execute("SELECT * FROM speed_measuring_sensor")
 rows = cur.fetchall()
-print rows
+for row in rows:
+    print (row)
 
